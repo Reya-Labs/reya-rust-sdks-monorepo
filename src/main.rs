4d211517c155eeffe330ac10821f3945c93c2143
michel@reya.xyz <michel@reya.xyz>
2024-06-12 12:25:10 +0200

    reorginized code and applied PR comments

--- src/main.rs
+++ src/main.rs
@@ -1,143 +1,35 @@
+mod reya_network;
+use crate::reya_network::http_provider;
 use alloy::{
-    network::EthereumSigner,
-    primitives::{address, Address, Bytes, I256, U256},
-    providers::ProviderBuilder,
+    primitives::{I256, U256},
     signers::wallet::LocalWallet,
-    sol,
 };
-use alloy_sol_types::SolValue;
 use eyre;
 use std::env;
 use tokio;
 use url::Url;
-//use futures_util::{future, StreamExt};
-//use futures::task::Poll;
-
-// Codegen from ABI file to interact with the contract.
-sol!(
-    #[allow(missing_docs)]
-    #[sol(rpc)]
-    CoreProxy,
-    "transactions/abi/CoreProxy.json"
-);
-
-sol!(
-    #[allow(missing_docs)]
-    #[sol(rpc)]
-    rUSDProxy,
-    "transactions/abi/rUsdProxy.json"
-);
-
-#[repr(u8)]
-#[derive(Debug)]
-enum CommandType {
-    Deposit = 0,
-    Withdraw,
-    DutchLiquidation,
-    MatchOrder,
-    TransferMarginAccount,
-}
-
-static CORE_CONTRACT_ADDRESS: &str = "0xA763B6a5E09378434406C003daE6487FbbDc1a80";
-
-#[derive(Debug)]
-struct HttpProvider {
-    url: Url,
-}
-
-impl HttpProvider {
-    pub fn new(http_url: &Url) -> HttpProvider {
-        HttpProvider {
-            url: http_url.clone(),
-        }
-    }
-
-    pub async fn create_account(
-        &self,
-        signer: LocalWallet,
-        account_owner_address: &Address,
-    ) -> eyre::Result<Option<Address>> {
-        // create http provider
-        let provider = ProviderBuilder::new()
-            .with_recommended_fillers()
-            .signer(EthereumSigner::from(signer))
-            .on_http(self.url.clone());
-
-        // core create account
-        let core_proxy = CoreProxy::new(CORE_CONTRACT_ADDRESS.parse()?, provider);
-        let builder = core_proxy.createAccount(account_owner_address.clone());
-
-        let receipt = builder.send().await?.get_receipt().await?;
-
-        eyre::Ok(receipt.contract_address)
-    }
-
-    pub async fn execute(
-        &self,
-        signer: LocalWallet,
-        account_id: u128,
-        market_id: u128,
-        exchange_id: u128,
-        order_base: I256,        // side(+/- = buy/sell) + volume i256
-        order_price_limit: U256, // order price u256
-    ) -> eyre::Result<Option<Address>> {
-        // create http provider
-        let provider = ProviderBuilder::new()
-            .with_recommended_fillers()
-            .signer(EthereumSigner::from(signer))
-            .on_http(self.url.clone());
-
-        // core create account
-        let core_proxy = CoreProxy::new(CORE_CONTRACT_ADDRESS.parse()?, provider);
-
-        let order_price_limit_bytes = order_price_limit.to_le_bytes::<32>();
-        let order_base_bytes = order_base.to_le_bytes::<32>();
-
-        let volume_price_bytes = vec![order_base_bytes, order_price_limit_bytes];
-
-        // construct core proxy command struct
-        let command_type = CommandType::MatchOrder;
-
-        let command = CoreProxy::Command {
-            commandType: command_type as u8,                      //
-            inputs: Bytes::from(volume_price_bytes.abi_encode()), //
-            marketId: market_id,                                  //
-            exchangeId: exchange_id,                              //
-        };
-
-        let builder = core_proxy.execute(account_id, vec![command]);
-        let transaction_result = builder.send().await?;
-        let receipt = transaction_result.get_receipt().await?;
-
-        if receipt.inner.is_success() {
-            println!("Execute logs:{:?}", receipt.inner.logs());
-            // this does not return the 'output' data from the execute call, not sure where to get it from :(
-        }
-        eyre::Ok(receipt.contract_address)
-    }
-}
 
 #[tokio::main]
+#[allow(dead_code)]
 async fn main() -> eyre::Result<()> {
     let url = Url::parse("https://rpc.reya.network")?;
-    let http_provider: HttpProvider = HttpProvider::new(&url);
+    let http_provider: http_provider::HttpProvider = http_provider::HttpProvider::new(&url);
 
     let private_key = env::var("PRIVATE_KEY").unwrap();
-    let account_owner_address = address!("f8f6b70a36f4398f0853a311dc6699aba8333cc1");
+    /*
+        // create account
+        {
+            let account_owner_address = address!("f8f6b70a36f4398f0853a311dc6699aba8333cc1");
+            let signer: LocalWallet = private_key.parse().unwrap();
+            let account_id = http_provider
+                .create_account(signer, &account_owner_address)
+                .await;
 
-    // create account
-    {
-        let signer: LocalWallet = private_key.parse().unwrap();
-        let account_id = http_provider
-            .create_account(signer, &account_owner_address)
-            .await;
-
-        println!("Created account, account_id:{:?}", account_id);
-    }
-
+            println!("Created account, account_id:{:?}", account_id);
+        }
+    */
     // execute order
     {
-        // todo get correct market and exchange id
         let signer: LocalWallet = private_key.parse().unwrap();
 
         let account_id = 734u128; // externaly provided by trading party
