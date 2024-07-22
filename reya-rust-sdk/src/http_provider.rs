be72e4e29d34630e75c42b797afb9c91a1ccfe69
michel@reya.xyz <michel@reya.xyz>
2024-07-22 14:30:51 +0200

    set  gas limit to 20.000.000

--- reya-rust-sdk/src/http_provider.rs
+++ reya-rust-sdk/src/http_provider.rs
@@ -373,6 +373,7 @@ impl HttpProvider {
             signatures
         );
         let builder = proxy.batchExecute(orders, signatures);
+        let _ = builder.clone().gas(20_000_000);
         let transaction_result = builder.send().await?;
 
         let receipt = transaction_result.get_receipt().await?;
